fed773d363480c24bf18af0cdc77f7e9e27e35b4
Anvar Vahobov <vahobovanvar1@gmail.com>
2023-04-05 16:21:43 +0500

    эта страница для реализации созданых классов и с выводом решении задачи

--- public/index.php
+++ public/index.php
@@ -1,1 +1,29 @@
 <?php
+require_once('../vendor/autoload.php');
+$host = "localhost";
+$dbUser = "root";
+$dbPassword = "pass";
+$dbName = "booking";
+$cabinetNumber = 1;
+$bookingTime = "2021-08-12 15:01:00";
+$contact = "test@mail.com, +992921234567";
+
+$booking = new \App\Booking($host, $dbUser, $dbPassword, $dbName);
+// проверка свободности кабинета
+
+$result = $booking->checkAvailability($cabinetNumber, $bookingTime);
+if ($result === true) {
+    echo "Кабинет свободен, можно забронировать \n";
+} else {
+    echo $result;
+}
+
+// бронирование кабинета
+if ($result === true) {
+    $result = $booking->bookCabinet($cabinetNumber, $bookingTime, $contact);
+    if ($result === true) {
+        echo "Кабинет успешно забронирован";
+    } else {
+        echo $result;
+    }
+}
