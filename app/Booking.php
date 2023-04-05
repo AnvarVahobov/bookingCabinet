3d4007e4ee7d7641b762cc8d61226e7f974bf5fc
Anvar Vahobov <vahobovanvar1@gmail.com>
2023-04-05 16:19:58 +0500

    Добавить методы для класса Booking
    в методах данного класса при использовании методов класса Cabinet осуществляется проверка и бронирование кабинета
    
    checkAvailability проверяет доступность выбранного кабинета в выбранное время
    bookCabinet метод для проверки доступности кабинета и бронирования

--- app/Booking.php
+++ app/Booking.php
@@ -1,7 +1,46 @@
 <?php
-
+namespace App;
 
 class Booking
 {
+    private $cabinets = [];
+    private $conn;
 
+    public function __construct($host, $user, $password, $database)
+    {
+        $this->conn = mysqli_connect($host, $user, $password, $database);
+        for ($i = 1; $i <= 5; $i++) {
+            $this->cabinets[$i] = new Cabinet($i, $this->conn);
+        }
+    }
+
+    public function checkAvailability($number, $time)
+    {
+        if (isset($this->cabinets[$number])) {
+            $cabinet = $this->cabinets[$number];
+            if ($cabinet->isFree($time)) {
+                return true;
+            } else {
+                return $cabinet->getInfo();
+            }
+        } else {
+            return "Кабинет не найден";
+        }
+    }
+
+    public function bookCabinet($number, $time, $contact)
+    {
+        if (isset($this->cabinets[$number])) {
+            $cabinet = $this->cabinets[$number];
+            if ($cabinet->isFree($time)) {
+                $cabinet->book($time, $contact);
+                $cabinet->notify();
+                return true;
+            } else {
+                return $cabinet->getInfo();
+            }
+        } else {
+            return "Кабинет не найден";
+        }
+    }
 }
